2a9c9b067324afd7b520ad91fb819a6594416482
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2020-11-30 15:52:21 +0100

    correction

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,21 +4,22 @@ import React, { useState, useEffect } from 'react';
 // == Import
 import './styles.css';
 
+// mon tableau de dates
+let allDate = [];
+
 // == Composant
 const App = () => {
   /** Nombre d'envoi */
   const [counter, setCounter] = useState(0);
   /** Date */
-  const [timer, setTimer] = useState(null);
-
-  // mon tableau de dates
-  const allDate = [];
+  const [timer, setTimer] = useState('');
 
   useEffect(() => {
     allDate.push(timer);
   }, [timer]);
 
-  const handleClicButton = () => {
+  const handleClicButton = (e) => {
+    e.preventDefault();
     setCounter(counter + 1);
     setTimer(new Date().toLocaleDateString());
   };
@@ -28,17 +29,14 @@ const App = () => {
 
   return (
     <div className="app">
-
       <h1>Combien de cv as-tu envoyé aujourd'hui ?</h1>
-      <button
-        className=""
-        onClick={() => {
-          handleClicButton();
-        }}
-      >+1
+      <button className="" onClick={handleClicButton}>
+        +1
       </button>
       <p>Total : {counter}</p>
-      <p>Date : {allDate}</p>
+      {allDate.map((gDate) => (
+        <p> Date : {gDate} </p>
+      ))}
     </div>
   );
 };
