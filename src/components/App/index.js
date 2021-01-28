2a45a11feb75e9107af0e35a52e13fd9a2477417
Dimitri Basseguy <dimitri.basseguy@gmail.com>
2021-01-28 16:15:35 +0100

    localstorage ok (pas propre, mais ça marche)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,37 +4,58 @@ import React, { useState, useEffect } from 'react';
 // == Import
 import './styles.scss';
 
-const dateFormat = require('dateformat');
-
-// mon tableau de dates
-const allDate = [];
-
 // == Composant
 const App = () => {
+  // initialisation du localstore
+  if (localStorage.getItem('Date') === null) {
+    localStorage.setItem('Date', JSON.stringify([]));
+  }
+
+  const initialCounter = () => Number(localStorage.getItem('count') || 0);
   /** Nombre d'envoi */
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(initialCounter);
   /** Stamp Timer */
-  const [timer, setTimer] = useState('');
+  const startDate = new Date().toString();
+  const [timer, setTimer] = useState(startDate);
+  const dateStorage = localStorage.getItem('Date');
+  const dates = JSON.parse(dateStorage);
 
   useEffect(() => {
-    allDate.push(dateFormat(timer, 'dddd, mmmm dS, yyyy, h:MM:ss TT'));
+    localStorage.setItem('Date', JSON.stringify(dates));
+    localStorage.setItem('count', counter);
   }, [timer]);
 
   const handleClicButton = (e) => {
     e.preventDefault();
-    setCounter(counter + 1);
-    setTimer(new Date());
+
+    setTimer(new Date().toString());
+
+    const ids = dates.map((date) => date.id);
+    let idMax = 0;
+    if (ids.length > 0) {
+      idMax = Math.max(...ids);
+    }
+
+    const newDate = {
+      id: idMax + 1,
+      date: timer,
+    };
+
+    dates.push(newDate);
+    setCounter(dates.length);
+    localStorage.setItem('Date', JSON.stringify(dates));
+    localStorage.setItem('count', counter);
   };
 
   return (
     <div className="app">
-      <h1>Combien de cv as-tu envoyé aujourd'hui ?</h1>
-      <button className="btn-big" onClick={handleClicButton}>
+      <h1>Compteur de CV envoyés</h1>
+      <button type="submit" className="btn-grad" onClick={handleClicButton}>
         +1
       </button>
       <p>Total : {counter}</p>
-      {allDate.map((gDate) => (
-        <p> Date : {gDate} </p>
+      {dates.map((date) => (
+        <p key={date.id}> Id: {date.id} Date : {date.date} </p>
       ))}
     </div>
   );
